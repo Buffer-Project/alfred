aad16aa62b541f5af174a5dbc7f479ee94a4ba52
Ger <saltairac@uade.edu.ar>
2020-08-31 19:56:53 -0300

    setup front and back

---
+++ alfred-api/src/test/java/com/buffer/alfred/AlfredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buffer.alfred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
